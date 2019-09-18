65200d2177f9f06d63314daae4deb21f4881bda0
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2019-09-18 08:36:15 +0600

    initial commit

---
+++ app/src/main/java/com/example/donate/EventPost.java
@@ -0,0 +1,14 @@
+package com.example.donate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_post);
+    }
+}
---
+++ app/src/main/java/com/example/donate/PostInformation.java
@@ -0,0 +1,14 @@
+package com.example.donate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_information);
+    }
+}
