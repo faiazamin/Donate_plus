8606aff9bd05062edaef1052a6b4684fae2e02f2
sbrotee63 <sbrotee@gmail.com>
2019-10-02 04:57:58 +0600

    4th commit

--- app/src/main/java/com/sbrotee63/donate/SignUp.java
+++ app/src/main/java/com/sbrotee63/donate/SignUp.java
@@ -1,14 +1,76 @@
 package com.sbrotee63.donate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
+
+    String name;
+    String email;
+    String password;
+    String confirmPassword;
+    String bloodGroup;
+    String dateOfBirth;
+    String address;
+    String cellNumber;
+    String lastBloodDonation;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        EditText nameF = (EditText) findViewById(R.id.signup_text_name);
+        EditText emailF = (EditText) findViewById(R.id.signup_text_email);
+        EditText passwordF = (EditText) findViewById(R.id.signup_password_password);
+        EditText confirmPasswordF = (EditText) findViewById(R.id.signup_password_confirmpassword);
+        EditText bloodGroupF = (EditText) findViewById(R.id.signup_text_bloodgroup);
+        EditText dateOfBirthF = (EditText) findViewById(R.id.signup_text_dateofbirth);
+        EditText addressF = (EditText) findViewById(R.id.signup_text_address);
+        EditText cellNumberF = (EditText) findViewById(R.id.singup_text_cellnumber);
+        EditText lastBloodDonationF = (EditText) findViewById(R.id.signup_text_lastblooddonation);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+    private void signUp(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener <AuthResult>()
+        {
+            public void onComplete(@NonNull Task <AuthResult> task)
+            {
+                if(task.isSuccessful()){
+                    Log.d("DONATE+", "createUserWithEmail: successful");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }
+                else{
+                    Log.w("DONATE+", "createUserWithEmail: failure", task.getException());
+                    Toast.makeText(SignUp.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /*
+    public void onStart()
+    {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+     */
+
 }
