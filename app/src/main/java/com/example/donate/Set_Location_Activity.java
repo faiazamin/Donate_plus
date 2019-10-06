22bf8788397a228067439756861d57a4ab0eabad
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2019-10-06 13:20:39 +0600

    Add Map 3

--- app/src/main/java/com/example/donate/Set_Location_Activity.java
+++ app/src/main/java/com/example/donate/Set_Location_Activity.java
@@ -1,15 +1,19 @@
 package com.example.donate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Set_Location_Activity extends AppCompatActivity implements OnMapReadyCallback {
+public class Set_Location_Activity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -20,13 +24,17 @@ public class Set_Location_Activity extends AppCompatActivity implements OnMapRea
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(Set_Location_Activity.this);
+        onMapReady(mMap);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this,"Map is ready",Toast.LENGTH_SHORT).show();
         mMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
     }
 }
