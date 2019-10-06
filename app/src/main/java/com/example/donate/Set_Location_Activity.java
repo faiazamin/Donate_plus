b10dad574c43f1df8d0fb137decd174b68f5cd1a
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2019-10-06 10:17:58 +0600

    Add Map 2

--- app/src/main/java/com/example/donate/Set_Location_Activity.java
+++ app/src/main/java/com/example/donate/Set_Location_Activity.java
@@ -3,12 +3,30 @@ package com.example.donate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class Set_Location_Activity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class Set_Location_Activity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set__location_);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this,"Map is ready",Toast.LENGTH_SHORT).show();
+        mMap = googleMap;
+
     }
 }
