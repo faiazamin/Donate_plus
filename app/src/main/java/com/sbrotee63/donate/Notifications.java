0a6aec0b3cb55952ed57dbe0de50ea0af29d1fe7
sbrotee63 <sbrotee@gmail.com>
2019-10-14 16:29:17 +0600

    SignUp LogIn faults corrected

---
+++ app/src/main/java/com/sbrotee63/donate/Notifications.java
@@ -0,0 +1,14 @@
+package com.sbrotee63.donate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
+}
