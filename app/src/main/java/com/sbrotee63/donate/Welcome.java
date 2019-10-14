d836e8260c09a43c43eeb7e742d183e52d345960
sbrotee63 <sbrotee@gmail.com>
2019-10-15 02:55:27 +0600

    SignUp LogIn faults corrected

--- app/src/main/java/com/sbrotee63/donate/Welcome.java
+++ app/src/main/java/com/sbrotee63/donate/Welcome.java
@@ -2,7 +2,9 @@ package com.sbrotee63.donate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        findViewById(R.id.welcome_button_signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.welcome_button_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this, LogIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
