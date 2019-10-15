7b4fd256325ea547c825b1815235d239ef04d496
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2019-10-16 04:17:54 +0600

    Merge branch 'Branch_1' of https://github.com/faiazamin/Donate_plus into Faiaz
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/drawable/solid_btn_red.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml
    #	build.gradle
    #	gradle/wrapper/gradle-wrapper.properties
    #	gradlew
    #	gradlew.bat

--- app/src/main/java/com/sbrotee63/donate/NewsFeed.java
+++ app/src/main/java/com/sbrotee63/donate/NewsFeed.java
@@ -3,9 +3,11 @@ package com.sbrotee63.donate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class NewsFeed extends AppCompatActivity {
 
+    //private Button newFeedButton = (Button) findViewById(R.id.feed_button_feed);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
