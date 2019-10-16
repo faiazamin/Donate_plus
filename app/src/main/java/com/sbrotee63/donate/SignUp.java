9846ff27fe7739b90674b8f3d4f5852d4a7692fa
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2019-10-16 21:32:30 +0600

    Merge branch 'Branch_1' of https://github.com/faiazamin/Donate_plus into Faiaz
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/drawable/solid_btn_red.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml
    #	build.gradle
    #	gradle/wrapper/gradle-wrapper.properties
    #	gradlew
    #	gradlew.bat

--- app/src/main/java/com/sbrotee63/donate/SignUp.java
+++ app/src/main/java/com/sbrotee63/donate/SignUp.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -29,26 +30,38 @@ public class SignUp extends AppCompatActivity {
     String cellNumber;
     String lastBloodDonation;
 
+    EditText nameF = (EditText) findViewById(R.id.signup_text_name);
+    EditText emailF = (EditText) findViewById(R.id.signup_text_email);
+    EditText passwordF = (EditText) findViewById(R.id.signup_password_password);
+    EditText confirmPasswordF = (EditText) findViewById(R.id.signup_password_confirmpassword);
+    EditText bloodGroupF = (EditText) findViewById(R.id.signup_text_bloodgroup);
+    EditText dateOfBirthF = (EditText) findViewById(R.id.signup_text_dateofbirth);
+    EditText addressF = (EditText) findViewById(R.id.signup_text_address);
+    EditText cellNumberF = (EditText) findViewById(R.id.singup_text_cellnumber);
+    EditText lastBloodDonationF = (EditText) findViewById(R.id.signup_text_lastblooddonation);
+
     private FirebaseAuth mAuth;
+
+    private Button signUpButton = (Button) findViewById(R.id.signup_button_signup);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        EditText nameF = (EditText) findViewById(R.id.signup_text_name);
-        final EditText emailF = (EditText) findViewById(R.id.signup_text_email);
-        final EditText passwordF = (EditText) findViewById(R.id.signup_password_password);
-        EditText confirmPasswordF = (EditText) findViewById(R.id.signup_password_confirmpassword);
-        EditText bloodGroupF = (EditText) findViewById(R.id.signup_text_bloodgroup);
-        EditText dateOfBirthF = (EditText) findViewById(R.id.signup_text_dateofbirth);
-        EditText addressF = (EditText) findViewById(R.id.signup_text_address);
-        EditText cellNumberF = (EditText) findViewById(R.id.singup_text_cellnumber);
-        EditText lastBloodDonationF = (EditText) findViewById(R.id.signup_text_lastblooddonation);
+        nameF = (EditText) findViewById(R.id.signup_text_name);
+        emailF = (EditText) findViewById(R.id.signup_text_email);
+        passwordF = (EditText) findViewById(R.id.signup_password_password);
+        confirmPasswordF = (EditText) findViewById(R.id.signup_password_confirmpassword);
+        bloodGroupF = (EditText) findViewById(R.id.signup_text_bloodgroup);
+        dateOfBirthF = (EditText) findViewById(R.id.signup_text_dateofbirth);
+        addressF = (EditText) findViewById(R.id.signup_text_address);
+        cellNumberF = (EditText) findViewById(R.id.singup_text_cellnumber);
+        lastBloodDonationF = (EditText) findViewById(R.id.signup_text_lastblooddonation);
 
         mAuth = FirebaseAuth.getInstance();
 
-        findViewById(R.id.signup_button_signup).setOnClickListener(new View.OnClickListener() {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 signUp(emailF.getText().toString().trim(), passwordF.getText().toString().trim());
@@ -57,6 +70,7 @@ public class SignUp extends AppCompatActivity {
     }
 
     private void signUp(String email, String password){
+        if(!allFilledAndValidated()) return;
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener <AuthResult>()
         {
             public void onComplete(@NonNull Task <AuthResult> task)
@@ -75,6 +89,28 @@ public class SignUp extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private Boolean allFilledAndValidated(){
+        name = nameF.getText().toString().trim();
+        email = emailF.getText().toString().trim();
+        password = emailF.getText().toString().trim();
+        confirmPassword = confirmPasswordF.getText().toString().trim();
+        bloodGroup = bloodGroupF.getText().toString().trim();
+        dateOfBirth = dateOfBirthF.getText().toString().trim();
+        address = addressF.getText().toString().trim();
+        cellNumber = cellNumberF.getText().toString().trim();
+        lastBloodDonation = lastBloodDonationF.getText().toString().trim();
+        if(name.equals("") || email.equals("") || password.equals("") || confirmPassword.equals("") || bloodGroup.equals("") || dateOfBirth.equals("") || address.equals("") || cellNumber.equals("") || lastBloodDonation.equals("")){
+            Toast.makeText(SignUp.this, "Please fill all fields", Toast.LENGTH_SHORT);
+            return false;
+        }
+        if(!password.equals(confirmPassword)){
+            Toast.makeText(SignUp.this, "Passwords don't match", Toast.LENGTH_SHORT);
+            return false;
+        }
+        switch (bloodGroup){}
+        return true;
     }
 
     /*
