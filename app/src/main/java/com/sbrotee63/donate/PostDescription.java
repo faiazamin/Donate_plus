66049e0d6a07b3004cf8c9dfd8bb7b003bbb2b02
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2019-10-28 02:54:22 +0600

    Working on pot respons.

--- app/src/main/java/com/sbrotee63/donate/PostDescription.java
+++ app/src/main/java/com/sbrotee63/donate/PostDescription.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class PostDescription extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_description);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+
+
+
     }
 }
