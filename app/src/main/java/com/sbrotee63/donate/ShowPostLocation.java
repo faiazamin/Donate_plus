7b4072c10e227c9707c857b72aab45d4bf78a789
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2019-10-31 23:40:35 +0600

    map permissions  added .

--- app/src/main/java/com/sbrotee63/donate/ShowPostLocation.java
+++ app/src/main/java/com/sbrotee63/donate/ShowPostLocation.java
@@ -1,7 +1,14 @@
 package com.sbrotee63.donate;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,26 +18,73 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ShowPostLocation extends FragmentActivity implements OnMapReadyCallback {
+public class ShowPostLocation extends FragmentActivity {
 
     private GoogleMap mMap;
+    private boolean mLocationPermission = false;
+    private static final int LOCATION_PERMISSION = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_post_location);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        getLocationPermission();
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                // Add a marker in Sydney, Australia, and move the camera.
+                LatLng sydney = new LatLng(-34, 151);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            }
+        });
+
+
+
+    }
+    private void getLocationPermission(){
+        String[] permissions ={
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION        } ;
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[0]) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[1]) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermission = true;
+            }
+            else{
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        1234);
+            }
+        }
+        else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    1234);
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermission = false;
+
+        if(grantResults.length > 0 ){
+            for(int i = 0; i < grantResults.length; ++i) {
+                if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                    mLocationPermission = false;
+                    return;
+                }
+            }
+
+            mLocationPermission = true;
+            initMap();
+        }
     }
 }
