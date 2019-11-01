bd3b5201d53cce115295e2a01a7f6b75fa87d235
Faiaz Amin <faiazamin.24csedu.040@gmail.com>
2019-11-01 07:30:21 +0600

    Mycuurrent location  added .

--- app/src/main/java/com/sbrotee63/donate/ShowPostLocation.java
+++ app/src/main/java/com/sbrotee63/donate/ShowPostLocation.java
@@ -3,26 +3,40 @@ package com.sbrotee63.donate;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class ShowPostLocation extends FragmentActivity {
 
     private GoogleMap mMap;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
     private boolean mLocationPermission = false;
     private static final int LOCATION_PERMISSION = 1234;
+    private static final float ZOOM = 15f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,43 +45,91 @@ public class ShowPostLocation extends FragmentActivity {
         getLocationPermission();
     }
 
-    private void initMap(){
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                mMap = googleMap;
-
-                // Add a marker in Sydney, Australia, and move the camera.
-                LatLng sydney = new LatLng(-34, 151);
-                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-            }
-        });
-
-
-
-    }
     private void getLocationPermission(){
         String[] permissions ={
                 Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION        } ;
+        Log.d("newTag","getLocationPermission invoked");
         if(ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[0]) == PackageManager.PERMISSION_GRANTED){
+            Log.d("newTag",permissions[0]);
             if(ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[1]) == PackageManager.PERMISSION_GRANTED){
+                Log.d("newTag",permissions[1]);
                 mLocationPermission = true;
+                initMap();
             }
             else{
                 ActivityCompat.requestPermissions(this,
                         permissions,
-                        1234);
+                        LOCATION_PERMISSION);
+                Log.d("newTag",permissions[1]+"revoked");
             }
         }
         else{
             ActivityCompat.requestPermissions(this,
                     permissions,
-                    1234);
+                    LOCATION_PERMISSION);
+            Log.d("newTag",permissions[0]+"revoked");
         }
     }
+
+    private void getDeviceLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        Log.d("newTag", "getDeviceLocation found");
+        try {
+            if (mLocationPermission) {
+                Task location = fusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Log.d("newTag", "location found");
+                            Location currentLocation = (Location) task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), ZOOM);
+                        } else {
+                            Log.d("newTag", "current location not found");
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException s) {
+            Log.d("newTag", "SecurityException found");
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d("newTag", "moveCamera found");
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void initMap() {
+        final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+                                    @RequiresApi(api = Build.VERSION_CODES.M)
+                                    @Override
+                                    public void onMapReady(GoogleMap googleMap) {
+                                        mMap = googleMap;
+                                        Log.d("newTag", "initMap invoked");
+
+                                        if (mLocationPermission) {
+                                            getDeviceLocation();
+
+                                            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                                                return;
+                                            }
+                                            mMap.setMyLocationEnabled(true);
+                                            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                }
+
+
+            }
+        }
+        );
+
+
+
+    }
+
 
 
 
@@ -84,6 +146,7 @@ public class ShowPostLocation extends FragmentActivity {
             }
 
             mLocationPermission = true;
+            Log.d("newTag","onRequestPermissionsResult invoked");
             initMap();
         }
     }
