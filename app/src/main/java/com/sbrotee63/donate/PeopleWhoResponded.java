b35d4762e82b705b7d32851911fcbae5a0940d88
sbrotee63 <sbrotee@gmail.com>
2019-11-03 04:11:52 +0600

    Added 2 extra activities.
    MyResponses and PeopleWhoResponded.
    
    UI pages done.
    
    Now the whole polishing remains.

---
+++ app/src/main/java/com/sbrotee63/donate/PeopleWhoResponded.java
@@ -0,0 +1,14 @@
+package com.sbrotee63.donate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeopleWhoResponded extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people_who_responded);
+    }
+}
---
+++ app/src/main/java/com/sbrotee63/donate/RecyclerViewAdapterPeopleWhoResponded.java
@@ -0,0 +1,80 @@
+package com.sbrotee63.donate;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapterPeopleWhoResponded extends RecyclerView.Adapter<RecyclerViewAdapterPeopleWhoResponded.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapterResponses";
+
+    private ArrayList<String> responses = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapterPeopleWhoResponded(Context context, ArrayList<String> aResponses) {
+        responses = aResponses;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_people_who_responded, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        //Log.d(TAG, "onBindViewHolder: called.");
+
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Log.d(TAG, "onClick: clicked on: " + responses.get(position));
+
+                Toast.makeText(mContext, responses.get(position), Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(mContext, PostDescription.class);
+                intent.putExtra("notification", responses.get(position));
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    private void setText(String s) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return responses.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView response;
+        LinearLayout parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            response = itemView.findViewById(R.id.peoplewhoresponded_text_responses);
+
+            parentLayout = itemView.findViewById(R.id.parent_layout_people_who_responded);
+        }
+
+        public void setText(String s) {
+        }
+    }
+}
+
